fbcd70d80dd17d6c39147a1b108c2b140a3b24a7
Tazaev <tonymurtazaev@gmail.com>
2022-07-20 03:31:21 -0400

    added employees table

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INTEGER NULL,
+    role_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
