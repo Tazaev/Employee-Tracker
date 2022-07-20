b1a28bce7c950fd231b47441f169cab18b183ed0
Tazaev <tonymurtazaev@gmail.com>
2022-07-20 03:32:22 -0400

    added role table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE employees (
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -16,4 +17,13 @@ CREATE TABLE employees (
     role_id INTEGER NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    department_id INTEGER NOT NULL,
+    salary DECIMAL(10,4) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
