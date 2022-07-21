51d4d8f5943430935e2a055e7c991f13ccb5c103
Tazaev <tonymurtazaev@gmail.com>
2022-07-21 03:13:18 -0400

    added proper db connection, still need to figure out.env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+// const consoleTable = require("console.table");
+const cTable = require("console.table");
 const mysql = require("mysql2");
 const figlet = require("figlet");
 require("dotenv").config();
@@ -10,9 +11,10 @@ const PORT = process.env.PORT || 3001;
 const db = mysql.createConnection(
   {
     host: "localhost",
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: "root",
+    password: "Darthjarjar",
+    database: "employee_db",
+    multipleStatements: true,
   },
   console.log(`you are now Connected to the database.`)
 );
@@ -28,25 +30,73 @@ console.log(
 );
 
 function startPromp() {
-  inquirer.prompt({
-    name: "do_what",
-    type: "rawlist",
-    message: "Greetings, below is a list of options for you to view",
-    choices: [
-      "View all Employees, Departments and Roles",
-      "View all Employees by Department",
-      "View all Employees by Role",
-      "View all Employees by Manager ID",
-      "Add Employee",
-      "Add Department",
-      "Add Role",
-      "Remove Employee",
-      "Update Employee Role",
-      "Remove Role",
-      "Remove Department",
-      "Update Employee Manager",
-      "Exit",
-    ],
-  });
+  inquirer
+    .prompt({
+      name: "do_what",
+      type: "rawlist",
+      message: "Greetings, below is a list of options for you to view",
+      choices: [
+        "View all Employees, Departments and Roles",
+        "View all Employees by Department",
+        "View all Employees by Role",
+        "View all Employees by Manager ID",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Remove Employee",
+        "Update Employee Role",
+        "Remove Role",
+        "Remove Department",
+        "Update Employee Manager",
+        "Exit",
+      ],
+    })
+    .then((answers) => {
+      switch (answers.do_what) {
+        case "View all Employees, Departments and Roles":
+          viewAll();
+          break;
+        case "View all Employees by Department":
+          viewByDep();
+          break;
+        case "View all Employees by Role":
+          viewByRole();
+          break;
+        case "View all Employees by Manager ID":
+          viewByManager();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Department":
+          addDepart();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Remove Employee":
+          removeEmployee();
+          break;
+        case "Update Employee Role":
+          updateRole();
+          break;
+        case "Remove Role":
+          removeRole();
+          break;
+        case "Remove Department":
+          removeDepart();
+          break;
+        case "Update Employee Manager":
+          updateManager();
+          break;
+        //if 'Exit' is chosen, end connection
+        case "Exit":
+          console.log("---------------------------");
+          console.log("All Done! Have a great day!");
+          console.log("---------------------------");
+          connection.end();
+          break;
+      }
+    });
 }
 startPromp();
